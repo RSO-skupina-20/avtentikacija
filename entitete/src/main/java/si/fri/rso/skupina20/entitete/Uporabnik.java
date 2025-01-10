eecd912dc5244c84c9965ca90e0a87d8bfc01677
jantopolovec <jantopolovec7@gmail.com>
2025-01-10 22:16:50 +0100

    Ingress

--- entitete/src/main/java/si/fri/rso/skupina20/entitete/Uporabnik.java
+++ entitete/src/main/java/si/fri/rso/skupina20/entitete/Uporabnik.java
@@ -10,6 +10,7 @@ import javax.persistence.*;
         @NamedQuery(name = "Uporabnik.deleteUporabnik", query = "DELETE FROM uporabnik p WHERE p.id = :id"),
         @NamedQuery(name = "Uporabnik.getUporabnikByEmail", query = "SELECT p FROM uporabnik p WHERE p.email = :email")
 })
+
 public class Uporabnik{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
